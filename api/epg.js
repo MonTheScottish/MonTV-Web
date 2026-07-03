a2eca09b5eb4646d34cc080a4289eddbc168f197
Hieu <hieu@montv.web>
2026-07-03 09:02:16 +0700

    feat: add vercel.json and api proxy handlers for Vercel deployment

---
+++ api/epg.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+  // Reconstruct path and query parameters
+  const path = req.query.path || "";
+  
+  // Get all query params except 'path'
+  const queryParams = new URLSearchParams(req.query);
+  queryParams.delete("path");
+  
+  const queryString = queryParams.toString();
+  const url = `https://vnepg.site/${path}${queryString ? "?" + queryString : ""}`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "OkHttp/4.9.2",
+        "Referer": "https://vnepg.site",
+      },
+    });
+
+    const contentType = response.headers.get("content-type");
+    if (contentType) {
+      res.setHeader("Content-Type", contentType);
+    }
+    
+    // Enable CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+
+    const data = await response.text();
+    return res.status(response.status).send(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
